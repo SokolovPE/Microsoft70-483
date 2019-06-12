5810543e33f058f93881fd44a7782ad0b79d44d5
SokolovP <wolfpw@yandex.ru>
2019-06-12 21:33:44 +0500

    First commit

---
+++ ThreadPool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem((s)=>{
+                System.Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace myApp
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+
+        public static ThreadLocal<int> _fieldLocal = new ThreadLocal<int>(()=>{
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+
+        public static void Main(){
+            Thread t1 = new Thread(()=>{
+                for(int i=0;i<10;i++){
+                    System.Console.WriteLine("Thread A: {0}", ++_field);
+                }
+            });
+
+            Thread t2 = new Thread(()=>{
+                for(int i=0;i<10;i++){
+                    System.Console.WriteLine("Thread B: {0}", ++_field);
+                }
+            });
+            t1.Start();
+            t1.Join();
+            t2.Start();
+            t2.Join();
+            Console.ReadKey();
+            System.Console.WriteLine();
+            new Thread(()=>{
+                for (int i = 0; i < _fieldLocal.Value; i++)
+                {
+                    System.Console.WriteLine("Thread A: {0}", i);
+                }
+            }).Start();
+            new Thread(()=>{
+                for (int i = 0; i < _fieldLocal.Value; i++)
+                {
+                    System.Console.WriteLine("Thread B: {0}", i);
+                }
+            }).Start();
+            Console.ReadKey();
+            System.Diagnostics.Debugger.Break();
+            
+#if DEBUG
+            Console.WriteLine("Debug");
+#endif
+            Console.ReadKey();
+        }
+    }
+}
