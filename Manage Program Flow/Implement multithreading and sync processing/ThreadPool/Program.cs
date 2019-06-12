a948aff779836c519e35398fedc53ed38b425724
SokolovPE <sokolovp@outlook.com>
2019-06-12 23:22:26 +0500

    ThreadPool updated

--- Manage Program Flow/Implement multithreading and sync processing/ThreadPool/Program.cs
+++ Manage Program Flow/Implement multithreading and sync processing/ThreadPool/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Threading;
 
-namespace ThreadPool
+namespace ThreadPools
 {
     class Program
     {
+        // Thread Pool is created to reuse threads you're finished to use
+        // Instead of letting thread die, you send it back to the pool
+        // where it can be reused
+
+        // Thread pool ensures that each request gets added to the queue and 
+        // when a thread becomes available, it is processed
+        // This ensures that your server doesn't crash under the amount of requests
+
+        // The thread pool automatically manages the amount of threads it needs to keep around
+        // When it's created it starts out empty. As a request comes in, it creates additional 
+        // threads to handle those requests
+
+        // Find more at: https://docs.microsoft.com/en-us/dotnet/api/system.threading.threadpool?view=netframework-4.8
+
         static void Main(string[] args)
         {
             ThreadPool.QueueUserWorkItem((s)=>{
